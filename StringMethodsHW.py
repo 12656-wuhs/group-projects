a1d74f94fea783f5fe667ee3bb77afab0953351c
12656-wuhs <12656@edu.wheatlandhigh.org>
2026-02-05 21:39:38 +0000

    cloned repository and began work on the group project

---
+++ StringMethodsHW.py
@@ -0,0 +1,3 @@
+#Method: Find and replace.
+#Situation: You run a business. One of your clients has sent you an email saying they submitted an order incorrectly, and now you \
+#must scan through yourr master file of clients and orders to find their order, and replace it with the correct one.
