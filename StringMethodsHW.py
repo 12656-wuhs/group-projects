8a86d813dedd2c4c8d346ca8c8efd9651fa255cd
12656-wuhs <12656@edu.wheatlandhigh.org>
2026-02-06 19:53:04 +0000

    Added pseudocode notes

--- StringMethodsHW.py
+++ StringMethodsHW.py
@@ -1,3 +1,9 @@
 #Method: Find and replace.
 #Situation: You run a business. One of your clients has sent you an email saying they submitted an order incorrectly, and now you \
-#must scan through yourr master file of clients and orders to find their order, and replace it with the correct one.
+#must scan through your master file of clients and orders to find their order, and replace it with the correct one.
+
+#variable = to a readline function to scan through the file line by line.
+#between each line switch, compare that variable to the clients name to check if its the right person.
+#find and replace function to then swap their order for something else, we'll figure out what later
+#May need to find a way to isolate the scope of the find and replace function, as it will swap out EVERYTHING in the file = to the criteria
+#Make a variable = to the line containing an order, modify that and then write it into the function.
