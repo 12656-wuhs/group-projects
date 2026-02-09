d7a1733d86040096c6a0400030da373a62471a1f
12656-wuhs <12656@edu.wheatlandhigh.org>
2026-02-09 06:14:50 +0000

    Wrote logic for StringMethodsHW.py

--- StringMethodsHW.py
+++ StringMethodsHW.py
@@ -1,3 +1,8 @@
+#Names: Logan Rasciner, Nathaniel Rodriguez, James Nickell, Jaidyn Li, James Lawson
+#Period: 6
+#Assignment: String Methods Group Project
+#Time Spent (LR): An hour and 45 minutes roughly
+
 #Method: Find and replace.
 #Situation: You run a business. One of your clients has sent you an email saying they submitted an order incorrectly, and now you \
 #must scan through your master file of clients and orders to find their order, and replace it with the correct one.
@@ -7,3 +12,64 @@
 #find and replace function to then swap their order for something else, we'll figure out what later
 #May need to find a way to isolate the scope of the find and replace function, as it will swap out EVERYTHING in the file = to the criteria
 #Make a variable = to the line containing an order, modify that and then write it into the function.
+import random
+items = ["Classic Sourdough",
+    "Roasted Garlic Batard",
+    "Seeded Rye Loaf",
+    "Heritage Whole Wheat",
+    "Rosemary & Sea Salt Focaccia",
+    "Kalamata Olive Ciabatta",
+    "Cranberry Walnut Pull-Apart",
+    "Jalapeño Cheddar Loaf",
+    "Brioche Nanterre",
+    "Honey Oat Sandwich Bread",
+    "French Baguette (Traditional)",
+    "Multi-Grain Boule",
+    "Pumpkin Seed Spelt",
+    "Chocolate Ganache Babka",
+    "Cinnamon Swirl Brioche",
+    "Everything Bagel 6-Pack",
+    "Sesame Semolina",
+    "Fig and Anise Loaf",
+    "Pumpernickel Round",
+    "English Muffin 4-Pack"
+]
+#generates new items to replace the old ones with
+newitem1 = random.choice(items)
+newitem2 = random.choice(items)
+newitem3 = random.choice(items)
+#concatenates all the new items together into one string for use with the .replace method.
+neworder = f'{newitem1}\n{newitem2}\n{newitem3}\n'
+
+def replaceorder(fulltext, oldtext, newtext):
+    replacedorder = fulltext.replace(oldtext, newtext)
+    return replacedorder
+    
+         
+
+def findorder():
+    global newitems
+    target = 'Graham, Alex'
+
+    with open('ClientList.txt', 'r') as clientnamesearch:
+            lines = clientnamesearch.readlines()
+
+            for line in range(len(lines)):
+                
+                if target in lines[line]:
+                    print(f'User found at line {line + 1}. Identifying old items...')
+                    #We know all orders make up three lines, meaning we can simply scan the next three lines and concatenate them together to find the old order.
+                    olditem1 = lines[line + 1].strip()
+                    olditem2 = lines[line + 2].strip()
+                    olditem3 = lines[line + 3].strip()
+                    #used .strip on each old item to ensure the concatenation proceeded smoothly.
+                    oldorder = f'{olditem1}\n{olditem2}\n{olditem3}\n'
+                    print(f'Old order identified. \n {oldorder} \n Replacing...')
+                    #with this this defintion has served its purpose will now pass data to the function responsible for actually altering the file.
+                    #mushing the list of lines created in the readlines method will also allow us to use .replace properly and not wipe out the entire text file
+                    fullfile = ''.join(lines)
+                    replacedwith = replaceorder(fullfile, oldorder, neworder)
+                    with open('ClientList.txt', 'w') as blahblah:
+                         blahblah.write(replacedwith)
+                         print('Replacment completed succesfully!')
+findorder()
