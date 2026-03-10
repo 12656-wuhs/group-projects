ff6207ab0fb34af725aeeb2f8956cdff031cfedd
12656-wuhs <12656@edu.wheatlandhigh.org>
2026-03-10 21:10:00 +0000

    Began development on the save and load function of the game

--- GP2/LR_Part.py
+++ GP2/LR_Part.py
@@ -1,4 +1,7 @@
 import os
+import pickle
+savecount = None
+balance = 100
 def main_menu():
     #Prints out a welcome message
     print(f'''Hello! Welcome to our humble casino, I hope you have a lovely time, and our options of games are as follows:
@@ -6,8 +9,8 @@ def main_menu():
 2: Blackjack
 3: Craps
 4: Roulette
-5: Quit''')
-    gamechoice = input('')
+5: Quit''') 
+    gamechoice = input('') #spits the input prompt onto an empty new line so things look clean
     if gamechoice == '1':
         slots() #Placeholder function name for the game that will go here
     elif gamechoice == '2':
@@ -24,6 +27,24 @@ def main_menu():
         return
 
 
+def loadsave(tokens, bootnum):
+    global balance
+    
+    with open('SaveState.txt', 'r') as loadgame:
+        loadgame.readlines
+        for line in loadgame:
+            if 'Tokens:' in line:
+                line.strip('Tokens:')
+                tokens = int(line)
+            if 'Save Count: ' in line:
+                line.strip('Save Count: ')
+                bootnum = int(line)
+    print('This feature is under construction')
 
-def savestate():
-    print('This function is still under construction.')
+def savestate(tokens, bootnum):
+    with open('SaveState.txt', 'w') as savegame:
+        objectstosave = {'Tokens':tokens, 'SaveCount':bootnum}
+
+
+def recordkeep():
+    print('Under construction')
