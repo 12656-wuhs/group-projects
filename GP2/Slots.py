6c779fdb18c31c0ecea337b366e8d399e3821aee
12664-star <12664@edu.wheatlandhigh.org>
2026-03-10 17:51:27 +0000

    Made adjustments to code to fix some problems

--- GP2/Slots.py
+++ GP2/Slots.py
@@ -3,22 +3,26 @@ import random
 credits = 999999999999999999999999999999
 
 def slots():
+    global credits
     symbols = ['🍒','🍋','🍇','BAR','7']
     combo_current = []
-    player_roll
+    player_roll = []
     combo_select = 3
     player_RNG = 3
-    print(f'Current Credits: {credits})
+    print(f'Current Credits: {credits}')
     while True:
         while True:
             try:
-                slots_bet = int(input('Place a bet: '))
-                if slots_bet > credits or slots_bet < 0:
-                    print(f'You do not have {slots_bet}. Please enter a valid bet.')
-                    continue
-                break
+                while True:
+                    slots_bet = int(input('Place a bet: '))
+                    if slots_bet > credits or slots_bet < 0:
+                        print(f'You do not have {slots_bet}. Please enter a valid bet.')
+                        continue
+                    else:
+                        break
             except ValueError:
                 print('Please enter a valid bet.')
+            break
         while combo_select > 3:
             select = random.choice(symbols)
             combo_current = combo_current + select
@@ -26,7 +30,7 @@ def slots():
         while player_RNG > 3:
             select = random.choice(symbols)
             player_roll = player_roll + select
-            player_RNG player_RNG - 1
+            player_RNG = player_RNG - 1
         if player_roll == combo_current:
             credits = credits + (slots_bet * 2)
             print('Jackpot!')
@@ -37,4 +41,5 @@ def slots():
                 elif again == 'N':
                     break
             break
-    
+
+slots()
