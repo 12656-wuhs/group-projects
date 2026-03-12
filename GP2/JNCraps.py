c59f959b02d18200aa27d9e740ef3002d5a66bc9
RikuTwT <nickelljames58@gmail.com>
2026-03-12 23:29:29 +0000

    Craps done

--- GP2/JNCraps.py
+++ GP2/JNCraps.py
@@ -13,6 +13,7 @@
 import random
 
 
+
 def roll_dice():
     return random.randint(1, 6) + random.randint(1, 6)
 
@@ -20,17 +21,18 @@ def roll_dice():
 
 
 def craps():
+    starting_balance = 1000
     # Establishes players
     try:
         player_count = int(input("How many players do you want in the game?: "))
     except ValueError:
         print("Defaulting to 1")
         player_count = 1
-        
+       
     players = []
     for i in range(player_count):
         name = input(f"Enter the name of your player {i+1}: ")
-        players.append({'name': name, 'balance': 1000})
+        players.append({'name': name, 'balance': starting_balance})
         
     shooter_index = 0
     
@@ -72,7 +74,6 @@ def craps():
                     print(f"\n{p['name']} Balance: ${p['balance']}")
                     bet = int(input("Place your bets: $"))
                     if 0 < bet <= p['balance']:
-                        p['balance'] -= bet
                         current_bets[p['name']] = bet
                         break
                     else:
@@ -86,37 +87,50 @@ def craps():
         point = roll_dice()
         print(f"You rolled a {point}")
         
-        round_won = False
-        point_phase = False
+        round_result = None
         
         # These will decide what happens dependent on what you roll on your come out roll
         if point == 7 or point == 11:
             print("YOU WIN, it's a natural, and so are you")
-            round_won = True
+            round_result = 'win'
         elif point == 2 or point == 3 or point == 12:
             print("You lose, you crapped out, you got it next time Queen or King")
-            round_won = False
+            round_result = 'lose'
         else:
             print(f"You didn't win or lose, you made it point which is {point}")
-            point_phase = True
             
         # The Second Phase
         print("Alrighty roo, now that you have 'point' you get to roll again.\nYour goal is to hit point BEFORE rolling a 7.")
-        if point_phase:
-            while True:
-                input("Press roll to enter again please...")
-                pass_line = roll_dice()
-                print(f"You rolled {pass_line}")
+        while True:
+            input("Press roll to enter again please...")
+            pass_line = roll_dice()
+            print(f"You rolled {pass_line}")
+            
+            
+            if pass_line == 7:
+                print("Well, you didn't get it this time. No worries tho, you'll get it next time.")
+                round_result = 'lose'
+                shooter_index += 1
+                break
+            elif pass_line == point:
+                print("You win, fantastic job. I'm proud of you")
+                round_result = 'win'
+                break
+            else:
+                print("Go ahead roll again.")
                 
                 
-                if pass_line == 7:
-                    print("Well, you didn't get it this time. No worries tho, you'll get it next time.")
-                    break
-                elif pass_line == point:
-                    print("You win, fantastic job. I'm proud of you")
-                    break
-                else:
-                    print("Go ahead roll again.")
+            # Payout 
+        for p in active_players:
+            bet = current_bets[p['name']]
+            if round_result == 'win':
+                p['balance'] += bet
+            else:
+                p['balance'] -= bet
+            print(f"{p['name']} now has ${p['balance']}")
+            
+            if input("\nPlay another round? (y/n): ").lower() != 'y':
+                break
         
         
         
